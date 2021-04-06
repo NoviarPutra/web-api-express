f18eef10bf199d8aed18585876203fb0a4ad761f
NoviarPutra <noviarpb@gmail.com>
2021-04-07 01:43:12 +0700

    knex config

--- src/api/config/db.config.js
+++ src/api/config/db.config.js
@@ -1,7 +1,4 @@
 const environment = process.env.DB_ENVIRONMENT || "development";
 const config = require("../../../knexfile")[environment];
-const knex = require("knex");
 
-const dbConnection = knex(config);
-
-module.exports = dbConnection;
+module.exports = require("knex")(config);
